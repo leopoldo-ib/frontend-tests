ea9008d4038733965af8dce73f2f5db0edb42826
leopoldo-ib <leopoldo@impossible-brands.com>
2023-09-21 18:10:57 +0200

    feat: pull out test method

--- .playwright/tests/contact-click.spec.ts
+++ .playwright/tests/contact-click.spec.ts
@@ -5,6 +5,9 @@ import { accessStoreWithPassword } from './__helpers__/flows';
 test.describe('Contact click', () => {
   test.beforeEach(async ({ page }) => {
     await accessStoreWithPassword(page)
+  })
+
+  test('matches the screenshot', async ({page})=> {
     await page.getByRole('link',{name: 'Contact'}).click() 
     await page.goto(config.ROUTES.CONTACT)
     await expect(page).toHaveScreenshot()
