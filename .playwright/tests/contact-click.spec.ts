9c11b65a2b9758387dbc3e0f8321ebe8b7364ebd
leopoldo-ib <leopoldo@impossible-brands.com>
2023-09-22 15:41:27 +0200

    feat: try to use already existing method

--- .playwright/tests/contact-click.spec.ts
+++ .playwright/tests/contact-click.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
 import config from '../config';
 import { accessStoreWithPassword } from './__helpers__/flows';
+import { fullPageHasScreenshot } from './__helpers__/assertions';
 
 test.describe('Contact click', () => {
   test.beforeEach(async ({ page }) => {
     await accessStoreWithPassword(page)
   })
 
-  test('matches the screenshot', async ({page})=> {
+  test('matches the screenshot', async ({ page }) => {
     await page.getByRole('link', { name: 'Contact' }).click();
     await page.goto(config.ROUTES.CONTACT)
-    await expect(page).toHaveScreenshot()
+    await fullPageHasScreenshot(page)
   })
 })
